e49e999c5ec8aef80ba6fa7718d87d18bf16ad70
Achilles Scheufele <ascheufe@1-C-18.42heilbronn.de>
2026-07-03 14:03:21 +0200

    Just for linter

--- a_maze_ing.py
+++ a_maze_ing.py
@@ -1,6 +1,7 @@
 from src.config import loading_setup
 from mazegen import MazeGenerator, write_maze
 from src.display import render_maze
+from src.mazegen.solver import Solver
 
 if __name__ == "__main__":
     cfg = loading_setup("config.txt")
@@ -13,10 +14,12 @@ if __name__ == "__main__":
         cfg.exit,
         cfg.output_file,
         cfg.perfect,
-        42   # ! keine seed attribute in Config bitte fixen danke
+        None   # ! keine seed attribute in Config bitte fixen danke
     )
     print(gen)
     gen.generate()
     print("\nGrid:", gen.grid, sep="\n")
-    write_maze(gen, ['P', 'E', 'N', 'I', 'S'])
-    render_maze(gen)
+    # write_maze(gen, ['P', 'E', 'N', 'I', 'S'])
+    solv = Solver(gen.grid)
+    path = solv.solver((0, 0), (30, 30))
+    render_maze(gen, path, True, "magenta", "red")
